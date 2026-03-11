a5911270ba578c6e0d43245f6abc0612045526c2
alessandro042 <20223TN058@utez.edu.mx>
2026-03-10 20:21:11 -0600

    First commit

---
+++ src/main/java/mx/edu/cecyte/api_visitas/ApiVisitasApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.cecyte.api_visitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVisitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVisitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/cecyte/api_visitas/ApiVisitasApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.cecyte.api_visitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVisitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
